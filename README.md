15e84ceb171e99a757c5968ce8b105b568f3c44e
D <dmitry@yashin.su>
2024-01-12 14:42:17 +0300

    add: support ngrok-javascript

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # node-red-contrib-ngrok
 
-This is a wrapper to expose and manage the popular tunnelling service [ngrok](https://ngrok.com) from Node-RED.
+This is a client ([ngrok-javascript](https://github.com/ngrok/ngrok-javascript)) to expose and manage the popular tunnelling service [ngrok](https://ngrok.com) from Node-RED.
 
-You can start and stop a tunnel using the button on the node or by injecting values,  the node will then output the ngrok host address as the `msg.payload`
+You can start and stop a tunnel using the button on the node or by injecting values, the node will then output the ngrok host address as the `msg.payload`
 
 The tunnel will expose the port that your node-red GUI is running on (eg 1880), you can override this port by setting the Port value in the node.
 
 The default ngrok region is US but you can also set Europe, Asia, Australia, South America, Japan or India.
 
-While ngrok doesn't require account registration if you register then you can have tunnels that live for more than 8 hour even on the free package, to get additional features such as reserved hostnames and multiple tunnels require a paid account.
-You can set your authtoken for your ngrok account as a config param, if you don't have an authtoken, you dont need to enter one (leave set to "Add new ...")
+While ngrok **require** account registration then you can have tunnels that live for more than 8 hour even on the free package, to get additional features such as reserved hostnames (1 free) and multiple tunnels require a paid account.
 
 Other optional fields around the host and port that the connection is forwarded to along with adding auth or setting up a TCP tunnel are configurable within the node, consult the build in help or the ngrok documentation for an explanation.
 
@@ -21,3 +20,20 @@ By running this node you will be exposing your node-red install to the public in
 read (https://nodered.org/docs/security)
 
 Feedback, Issues and PRs welcome on github.
+
+### Platform Support
+
+Pre-built binaries are provided on NPM for the following platforms:
+
+| OS         | i686 | x64 | aarch64 | arm |
+| ---------- | -----|-----|---------|-----|
+| Windows    |   ✓  |  ✓  |    *    |     |
+| MacOS      |      |  ✓  |    ✓    |     |
+| Linux      |      |  ✓  |    ✓    |  ✓  |
+| Linux musl |      |  ✓  |    ✓    |     |
+| FreeBSD    |      |  ✓  |         |     |
+| Android    |      |     |    ✓    |  ✓  |
+
+> **Note**
+> `ngrok-javascript`, and [ngrok-rust](https://github.com/ngrok/ngrok-rust/) which it depends on, are open source, so it may be possible to build them for other platforms.
+> * `Windows-aarch64` will be supported after the next release of [Ring](https://github.com/briansmith/ring/issues/1167).
--- ngrok.js
+++ ngrok.js
@@ -1,59 +1,78 @@
-const ng = require('ngrok');
+const ng = require('@ngrok/ngrok');
 
 module.exports = function (RED) {
   function ngrok(config) {
-    const region_types = ["us", "eu", "ap", "au", "sa", "jp", "in"];
-    const proto_types = ["http", "tcp"];
-    const bind_tls_types = ["https", "true", "http", "false", "both"];
+    const region_types = ['us', 'eu', 'ap', 'au', 'sa', 'jp', 'in'];
+    const proto_types = ['http', 'tcp'];
+    const bind_tls_types = ['https', 'true', 'http', 'false'];
 
     RED.nodes.createNode(this, config);
     var node = this;
     node.creds = RED.nodes.getNode(config.creds);
-    node.port = config.port || "";
-    node.portType = config.portType || "num";
-    node.host = config.host || "";
-    node.hostType = config.hostType || "localhost"
+    node.port = config.port || '';
+    node.portType = config.portType || 'num';
+    node.host = config.host || '';
+    node.hostType = config.hostType || 'localhost';
     node.region = config.region;
-    node.regionType = config.regionType || "us";
-    node.proto = config.proto || "http";
+    node.regionType = config.regionType || 'us';
+    node.proto = config.proto || 'http';
     node.bind_tls = config.bind_tls;
-    node.bind_tlsType = config.bind_tlsType || "https";
+    node.bind_tlsType = config.bind_tlsType || 'https';
     node.subdomain = config.subdomain;
-    node.subdomainType = config.subdomainType || "none";
+    node.subdomainType = config.subdomainType || 'none';
     node.auth = config.auth;
-    node.authType = config.authType || "str";
-    node.hostHeader = config.hostHeader || "";
-    node.hostHeaderType = config.hostHeaderType || "none";
+    node.authType = config.authType || 'str';
+    node.hostHeader = config.hostHeader || '';
+    node.hostHeaderType = config.hostHeaderType || 'none';
     if (RED.nodes.getNode(config.creds) == null) {
-      node.authtoken = "";
+      node.authtoken = '';
     } else {
       node.authtoken = RED.nodes.getNode(config.creds).credentials.authtoken;
     }
-    node.status({ fill: "grey", shape: "ring", text: "idle" });
+    node.listener = null;
+
+    node.status({fill: 'grey', shape: 'ring', text: 'idle'});
 
     node.on('input', function (msg) {
+      if (!node.authtoken || node.authtoken == '') {
+        node.error('Usage of ngrok requires a verified account and authtoken');
+        node.status({fill: 'red', shape: 'dot', text: 'error'});
+        return;
+      }
 
-      if (msg.payload == 'off' || msg.payload === false) {
+      if (['false', 'off', '0'].indexOf(String(msg.payload).toLowerCase()) != -1) {
         (async function () {
-          await ng.kill();
+          try {
+            if (node.listener) {
+              await node.listener.close();
+              node.listener = null;
+            } else {
+              // _subdomain
+              let _subdomain = RED.util.evaluateNodeProperty(node.subdomain, node.subdomainType, node, msg);
+              if (_subdomain.indexOf('.') > -1) {
+                await ng.disconnect(_subdomain);
+              }
+            }
+          } catch (_) {}
           msg.payload = null;
           node.send(msg);
-          node.status({ fill: "red", shape: "ring", text: "disconnected" });
+          node.status({fill: 'red', shape: 'ring', text: 'disconnected'});
         })();
-
-      } else if (msg.payload == 'on' || msg.payload == true) {
+      } else if (['true', 'on', '1'].indexOf(String(msg.payload).toLowerCase()) != -1) {
         let _port, _host, _proto, _region, _bind_tls, _auth, _host_header;
 
-        if(node.portType == "node-red" || node.portType == "") {
-          _port = null
+        if (node.portType == 'node-red' || node.portType == '') {
+          _port = null;
         } else {
           _port = RED.util.evaluateNodeProperty(node.port, node.portType, node, msg);
         }
 
-        if (!_port)  { _port = RED.settings.uiPort; } //ensure port is something
-        
-        if(node.hostType == "localhost" || node.hostType == "") {
-          _host = '127.0.0.1'
+        if (!_port) {
+          _port = RED.settings.uiPort;
+        } //ensure port is something
+
+        if (node.hostType == 'localhost' || node.hostType == '') {
+          _host = '127.0.0.1';
         } else {
           _host = RED.util.evaluateNodeProperty(node.host, node.hostType, node, msg);
         }
@@ -61,7 +80,8 @@ module.exports = function (RED) {
         if (proto_types.indexOf(node.proto) >= 0) {
           _proto = node.proto;
         } else {
-          node.error("Invalid protocol type", msg);
+          node.error('Invalid protocol type', msg);
+          node.status({fill: 'red', shape: 'dot', text: 'error'});
           return;
         }
 
@@ -71,7 +91,7 @@ module.exports = function (RED) {
           _region = RED.util.evaluateNodeProperty(node.region, node.regionType, node, msg);
         }
 
-        if (_proto !== "tcp" || _proto !== "http") {
+        if (String(_proto) === 'http') {
           //binding
           if (bind_tls_types.indexOf(node.bind_tlsType) >= 0) {
             _bind_tls = node.bind_tlsType;
@@ -81,58 +101,56 @@ module.exports = function (RED) {
           //subdomain
           _subdomain = RED.util.evaluateNodeProperty(node.subdomain, node.subdomainType, node, msg);
           //auth
-          if (node.authType == "none") {
+          if (node.authType == 'none') {
             _auth = null;
           } else {
             _auth = RED.util.evaluateNodeProperty(node.auth, node.authType, node, msg);
           }
           //host-header
-          if (node.hostHeaderType == "none") {
+          if (node.hostHeaderType == 'none') {
             _host_header = null;
           } else {
             _host_header = RED.util.evaluateNodeProperty(node.hostHeader, node.hostHeaderType, node, msg);
           }
           //ensure binding is valid
           switch (_bind_tls) {
-            case "false":
+            case 'false':
             case false:
-            case "https":
-              _bind_tls = ["https"]
-              break
-            case "http":
-              _bind_tls = ["http"]
-              break
-            case "both":
-              _bind_tls = ["http", "https"]
-              break
+            case 'https':
+              _bind_tls = ['https'];
+              break;
+            case 'http':
+              _bind_tls = ['http'];
+              break;
             default:
-              _bind_tls = ["https"] //default to secure
+              _bind_tls = ['https']; //default to secure
           }
         }
 
         var options = {
           authtoken: node.authtoken,
           proto: _proto,
-          addr: _host + ":"  +_port,
+          addr: _host + ':' + _port,
           region: _region,
           schemes: _bind_tls,
-          host_header: _host_header,
-        }
-        if (_subdomain.indexOf('.') > -1){
-          options.hostname = _subdomain
+          host_header: _host_header
+        };
+        if (_subdomain.indexOf('.') > -1) {
+          options.hostname = _subdomain;
         } else {
-          options.subdomain = _subdomain
+          options.subdomain = _subdomain;
         }
         if (_auth) {
           const auth = _auth.split(':');
           if (!auth || auth.length !== 2) {
-            node.error("ngrok auth should be formatted as user:password", msg);
+            node.error('ngrok auth should be formatted as username:password', msg);
+            node.status({fill: 'red', shape: 'dot', text: 'error'});
             return;
           }
-          if (auth[1].length < 8){
-            node.error("Password must be at least 8 characters")
+          if (auth[1].length < 8) {
+            node.error('Password must be at least 8 characters');
+            node.status({fill: 'red', shape: 'dot', text: 'error'});
             return;
-
           }
           options.basic_auth = _auth;
         }
@@ -141,16 +159,43 @@ module.exports = function (RED) {
         (async function () {
           try {
             //Disconnect once before reconnecting
-            await ng.kill();
-            const url = await ng.connect(options);
-            msg.payload = url;
+            if (node.listener) {
+              await node.listener.close();
+              node.listener = null;
+            } else {
+              if (options.hostname) {
+                await ng.disconnect(options.hostname);
+              }
+            }
+            node.listener = await ng.forward(options);
+            msg.payload = node.listener.url();
             node.send(msg);
-            node.status({ fill: "green", shape: "dot", text: url });
-          } catch ({ message }) {
-            console.log(`Connect error: ${message}`);
+            node.status({fill: 'green', shape: 'dot', text: msg.payload});
+          } catch ({message}) {
+            node.error(`Connect error: ${message}`);
+            node.status({fill: 'red', shape: 'dot', text: 'error'});
+            return;
           }
         })();
       }
+    });
+
+    node.on('close', function (removed, done) {
+      (async function () {
+        try {
+          if (node.listener) {
+            await node.listener.close();
+            node.listener = null;
+          } else {
+            // _subdomain
+            let _subdomain = RED.util.evaluateNodeProperty(node.subdomain, node.subdomainType, node);
+            if (_subdomain.indexOf('.') > -1) {
+              await ng.disconnect(_subdomain);
+            }
+          }
+        } catch (_) {}
+      })();
+      done();
     });
   }
 
@@ -160,33 +205,33 @@ module.exports = function (RED) {
     this.name = n.name;
   }
 
-  RED.nodes.registerType("ngrok", ngrok);
-  RED.nodes.registerType("ngrokauth", ngrokauth, {
+  RED.nodes.registerType('ngrok', ngrok);
+  RED.nodes.registerType('ngrokauth', ngrokauth, {
     credentials: {
-      authtoken: { type: "text" }
+      authtoken: {type: 'text'}
     }
   });
 
-  RED.httpAdmin.post("/ngrokInject/:id", RED.auth.needsPermission("inject.write"), function (req, res) {
+  RED.httpAdmin.post('/ngrokInject/:id', RED.auth.needsPermission('inject.write'), function (req, res) {
     var node = RED.nodes.getNode(req.params.id);
     if (node != null) {
       try {
-        var state = (req.body.on == 'true');
-        node.receive({ payload: state });
+        var state = req.body.on == 'true';
+        node.receive({payload: state});
         res.sendStatus(200);
       } catch (err) {
         res.sendStatus(500);
-        node.error(RED._("inject.failed", { error: err.toString() }));
+        node.error(RED._('inject.failed', {error: err.toString()}));
       }
     } else {
       res.sendStatus(404);
     }
   });
-}
+};
 
 function clean(obj) {
   for (var propName in obj) {
-    if (obj[propName] === null || obj[propName] === undefined || obj[propName] === "") {
+    if (obj[propName] === null || obj[propName] === undefined || obj[propName] === '') {
       delete obj[propName];
     }
   }
