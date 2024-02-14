df3e13f4a202fefc44d2da6c217a9b0d9b990d40
Sam Machin <sam@sammachin.com>
2024-02-14 16:11:45 +0000

    Update ngrok.js

--- ngrok.js
+++ ngrok.js
@@ -1,5 +1,6 @@
 const ng = require('@ngrok/ngrok');
 const Package = require('./package.json');
+const ngp = require('@ngrok/ngrok/package.json');
 
 module.exports = function (RED) {
   function ngrok(config) {
@@ -30,7 +31,6 @@ module.exports = function (RED) {
     node.listener = null;
 
     node.status({fill: 'grey', shape: 'ring', text: 'idle'});
-
     node.on('input', function (msg) {
       if (!node.authtoken || node.authtoken == '') {
         node.error('Usage of ngrok requires a verified account and authtoken');
@@ -57,7 +57,7 @@ module.exports = function (RED) {
           node.status({fill: 'red', shape: 'ring', text: 'disconnected'});
         })();
       } else if (['true', 'on', '1'].indexOf(String(msg.payload).toLowerCase()) != -1) {
-        let _port, _host, _proto, _bind_tls, _auth, _host_header;
+        let _port, _host, _proto, _domain, _bind_tls, _auth, _host_header;
 
         if (node.portType == 'node-red' || node.portType == '') {
           _port = null;
@@ -82,7 +82,7 @@ module.exports = function (RED) {
           node.status({fill: 'red', shape: 'dot', text: 'error'});
           return;
         }
-        if (String(_proto) === 'http') {
+        if (String(_proto) != 'tcp') {
           //binding
           if (bind_tls_types.indexOf(node.bind_tlsType) >= 0) {
             _bind_tls = node.bind_tlsType;
@@ -122,6 +122,7 @@ module.exports = function (RED) {
         const _red = RED.version();
         const _pname = Package.name.trim();
         const _pversion = Package.version.trim();
+        const _ngversion = ngp.version.trim();
         const _nname = node.name;
         const _nid = node.id;
 
@@ -129,7 +130,7 @@ module.exports = function (RED) {
           authtoken: node.authtoken,
           schemes: _bind_tls,
           host_header: _host_header,
-          session_metadata: `{"Node-RED":"${_red}","${_pname}":"${_pversion}","name":"${_nname},"id":"${_nid}"}`
+          session_metadata: `{"Node-RED":"${_red}","${_pname}":"${_pversion}","ngrok-javascript": ${_ngversion},"name":"${_nname},"id":"${_nid}"}`
         };
         if (_proto == 'https'){
           options.addr = "https://"+ _host + ':' + _port
@@ -171,6 +172,7 @@ module.exports = function (RED) {
                 await ng.disconnect(options.domain);
               }
             }
+            console.log(options)
             node.listener = await ng.forward(options);
             msg.payload = node.listener.url();
             node.send(msg);
